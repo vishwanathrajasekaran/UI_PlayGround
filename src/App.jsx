0f4d5b0f3ca4bc06fd433f728081229f1862a281
Vishwanath Rajasekaran <127646664+vishwanathrajasekaran@users.noreply.github.com>
2026-09-06 10:40:58 +0530

    Add Forms route to the main application

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar.jsx'
 import Home from './pages/Home.jsx'
 import BasicElements from './pages/basic-elements/BasicElements.jsx'
+import Forms from './pages/forms/Forms.jsx'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/basic-elements" element={<BasicElements />} />
+          <Route path="/forms" element={<Forms />} />
         </Routes>
       </main>
     </div>
